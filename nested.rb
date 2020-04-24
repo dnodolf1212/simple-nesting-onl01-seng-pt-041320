f1b37beb0b7f4c44e97083de719fd85aede72e7a
Dwight Nodolf <dnodolf@gmail.com>
2020-04-24 17:25:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,12 +15,12 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+  
 	
 	programmer_hash = 
  		{
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
+     
+     
 end
 
 def adding_matz
@@ -79,9 +83,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
-end
+  programmer_hash[:yukihiro_matsumoto] = {}
+end  
 
 def changing_alan
 	programmer_hash = 
@@ -121,5 +124,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] 
 end
